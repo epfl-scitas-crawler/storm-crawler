f05008ab3603593d2e961618add8647aecc613b3
Etienne Orliac <etienne.orliac@epfl.ch>
2022-03-12 20:16:01 +0100

    Setting up VPM

--- core/src/main/java/com/digitalpebble/stormcrawler/bolt/JSoupParserBolt.java
+++ core/src/main/java/com/digitalpebble/stormcrawler/bolt/JSoupParserBolt.java
@@ -5,13 +5,14 @@
  * you may not use this file except in compliance with the License. You may obtain a copy of the
  * License at
  *
- * <p>http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * Unless required by applicable law or agreed to in writing, software distributed under the
  * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.digitalpebble.stormcrawler.bolt;
 
 import static com.digitalpebble.stormcrawler.Constants.StatusStreamName;
@@ -177,9 +178,10 @@ public class JSoupParserBolt extends StatusEmitterBolt {
         String url = tuple.getStringByField("url");
         Metadata metadata = (Metadata) tuple.getValueByField("metadata");
 
-	/*
+	
         // If metadata.FORCE_ARCHIVE set to "yes", archive on Warc2StreamName,
         // return without parsing!
+	/*
         String forceArchive = (metadata.getFirstValue("FORCE_ARCHIVE"));
         LOG.info("forceArchive for parent homepage: {}? {}", url, forceArchive);
 
