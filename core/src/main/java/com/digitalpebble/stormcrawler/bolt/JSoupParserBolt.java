191b5148eb663fe59810500fb87575fde7e48055
Etienne Orliac <etienne.orliac@epfl.ch>
2022-03-13 00:12:27 +0100

    test

--- core/src/main/java/com/digitalpebble/stormcrawler/bolt/JSoupParserBolt.java
+++ core/src/main/java/com/digitalpebble/stormcrawler/bolt/JSoupParserBolt.java
@@ -12,7 +12,6 @@
  * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.digitalpebble.stormcrawler.bolt;
 
 import static com.digitalpebble.stormcrawler.Constants.StatusStreamName;
